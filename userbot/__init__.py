4b743f6e1504e8f571add83442f60f22d08dce49
BTetuko <55639773+BTetuko@users.noreply.github.com>
2020-04-08 22:51:50 +0700

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -113,9 +113,6 @@ ANTI_SPAMBOT_SHOUT = sb(os.environ.get("ANTI_SPAMBOT_SHOUT", "False"))
 # Youtube API key
 YOUTUBE_API_KEY = os.environ.get("YOUTUBE_API_KEY", None)
 
-# Quotes API Token
-QUOTES_API_TOKEN = os.environ.get("QUOTES_API_TOKEN", None)
-
 # Default .alive name
 ALIVE_NAME = os.environ.get("ALIVE_NAME", None)
 
